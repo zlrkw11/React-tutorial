6e4fcf51efe73620ee2b7e6c877f15bde6bb3c63
ZL <ray040406@gmail.com>
2023-12-02 19:14:26 +1300

    adding another square

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 export default function Square() {
-    return <button className="square">X</button>
+    return (
+        <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </>
+    
+    )
 }
 
